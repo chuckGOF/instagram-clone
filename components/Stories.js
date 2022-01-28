ae3c007ba5a06256363728b34536a6e9785c354b
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-01-28 18:38:06 +0000

    styling story component

--- components/Stories.js
+++ components/Stories.js
@@ -1,14 +1,30 @@
 import { useState, useEffect } from "react";
+import Story from "./Story";
+import faker from "@faker-js/faker";
 
 function Stories() {
 	const [profiles, setProfiles] = useState([]);
 
 	useEffect(() => {
-		fetch("https://fakerapi.it/api/v1/users?_quantity=10").then((resp) =>
-			console.log(resp.data)
-		);
+		const getProfiles = [...Array(20)].map((_, i) => ({
+			// x: faker.helpers.contextualCard()
+			...faker.helpers.contextualCard(),
+			id: i,
+		}));
+		setProfiles(getProfiles);
 	}, []);
-	return <div></div>;
+
+	return (
+		<div className="flex mt-5">
+			{profiles.map((profile) => (
+				<Story
+					key={profile.id}
+					img={profile.avatar}
+					username={profile.username}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default Stories;
--- components/Story.js
+++ components/Story.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function Story() {
-  return <div>
-      <h1>This is the story component</h1>
-  </div>;
+function Story({ username, img }) {
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<img
+				className="rounded-full h-10 w-10 object-contain"
+				src={img}
+				alt=""
+			/>
+			{/* <p>{username}</p> */}
+		</div>
+	);
 }
 
 export default Story;
